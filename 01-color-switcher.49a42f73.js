420192bb1ded3d30e1ecdf076fa3ffc76b20bc76
9Evhen7 <96841920+9Evhen7@users.noreply.github.com>
2022-06-18 11:16:31 +0000

    Deploying to gh-pages from @ 9Evhen7/goit-js-hw-09@1a47c6a91ff4731a938eee2a1782399d1b8843ab 🚀

---
+++ 01-color-switcher.49a42f73.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.body;let o=null;function d(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}n.style.height="100vw",n.style.width="100vw",t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n.style.backgroundColor=`${d()}`,o=setInterval((()=>{n.style.backgroundColor=`${d()}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.49a42f73.js.map
